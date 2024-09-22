e829f006754ca2e7e5fc4dba48cdd21f56ebaee3
dmcleary <dmcleary77@gmail.com>
2024-09-22 16:32:31 +0100

    Add simple database connection string

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
 
-echo "Hello World!";
+echo "<p>Attempting connection to database ...</p>";
+
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:discover202409221345.database.windows.net,1433; Database = discoverDbase", "azureuser", "B0rderl1ne_au");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+echo "<p>...Connection established!</p>"
+
+echo "<p>Hello World!</p>";
